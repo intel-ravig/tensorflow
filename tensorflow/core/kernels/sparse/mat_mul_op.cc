0e1dd3ccc81daac01154a87fa8b78061ef08f64c
Prashanth Palaniappan <prashanth.palaniappan@intel.com>
2022-01-24 11:16:03 -0800

    remove code in comments as per Google code requirements

--- tensorflow/core/kernels/sparse/mat_mul_op.cc
+++ tensorflow/core/kernels/sparse/mat_mul_op.cc
@@ -362,12 +362,10 @@ class CSRMatMulCPUOp : public CSRMatMulOp<CPUDevice, T> {
    matrix_descr descr;
    descr.type = SPARSE_MATRIX_TYPE_GENERAL;
    sparse_layout_t layout = SPARSE_LAYOUT_ROW_MAJOR;
-   //TODO: Default oneMKL sparse implementation is already optimized. So, the below function is not needed for now. This need to be turned on if we want to run a kernel selection logic in the future.
-   //status = mkl_sparse_set_mm_hint(mkl_matrix_handle, operation, descr, layout, num_rhs_cols, 100000 /* random large number */ );
+   //TODO: Default oneMKL sparse implementation is already optimized. So, "mkl_sparse_set_mm_hint" function is not needed for now. This need to be turned on if we want to run a kernel selection logic in the future.
    
    // Pick optimal kernel and other optimizations
-   //TODO: Default oneMKL sparse implementation is already optimized. So, the below function is not needed for now. This need to be turned on if we want to run a kernel selection logic in the future.
-   //status = mkl_sparse_optimize(mkl_matrix_handle);
+   //TODO: Default oneMKL sparse implementation is already optimized. So, "mkl_sparse_optimize" function is not needed for now. This need to be turned on if we want to run a kernel selection logic in the future.
    
    // Perform sparse matmul - output = A * input
    status = mkl_sparse_s_mm ( operation, 1.0f, mkl_matrix_handle, descr, layout, rhs.flat<float>().data(), num_rhs_cols, num_rhs_cols, 0.0f, output->flat<float>().data(), num_rhs_cols);
